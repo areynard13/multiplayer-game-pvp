ded74ccc868676416bcbd04493aea529da1f0d17
Adrien Reynard <reynard.adrien.08@gmail.com>
2025-05-19 14:02:40 +0200

    Ajout des joueurs

---
+++ src/components/CharacterController.jsx
@@ -0,0 +1,25 @@
+import { Joystick } from "playroomkit"
+import React, { useRef } from 'react';
+import { CharacterSoldier } from "./CharacterSoldier";
+import { useEffect, useState } from "react";
+
+export const CharacterController = ({
+        state,
+        joystick,
+        usePlayer,
+        ...props
+    }) => {
+        const group = useRef();
+        const character = useRef();
+        const [animation, setAnimation] = useState("Idle");
+        return (
+            <group ref={group} {...props}>
+                <group ref={character}>
+                    <CharacterSoldier
+                        color={state.state.profil?.color}
+                        animation={animation}
+                    />
+                </group>
+            </group>
+        )
+    }
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,9 +1,11 @@
 import { Environment, OrbitControls } from "@react-three/drei";
 import { Map } from "./Map";
-import { useEffect } from "react";
-import { insertCoin } from "playroomkit"
+import { useEffect, useState } from "react";
+import { insertCoin, Joystick, myPlayer, onPlayerJoin } from "playroomkit"
+import { CharacterController } from "./CharacterController";
 
 export const Experience = () => {
+  const [players, setPlayers] = useState([]);
 
   const start = async () => {
     await insertCoin();
@@ -11,26 +13,52 @@ export const Experience = () => {
 
   useEffect(() => {
     start();
+
+    onPlayerJoin((state) => {
+      const joystick = new Joystick(state, {
+        type: "angular",
+        buttons: [{ id: "fire", label: "Fire" }],
+      });
+
+      const newPlayer = { state, joystick }
+      state.setState("health", 100);
+      state.setState("deaths", 0);
+      state.setState("kills", 0);
+      setPlayers((players) => [...players, newPlayer]);
+
+      state.onQuit(() => {
+        setPlayers((players) => players.filter((p) => p.state.id !== state.id));
+      })
+    });
   }, []);
 
   return (
     <>
-    <directionalLight 
-    position={[25, 18, -25]}
-    intensity={0.7}
-    castShadow
-    shadow-camera-near={0}
-    shadow-camera-far={80}
-    shadow-camera-left={-30}
-    shadow-camera-right={30}
-    shadow-camera-top={25}
-    shadow-camera-bottom={-25}
-    shadow-mapSize-width={4096}
-    shadow-mapSize-height={4096}
-    shadow-bias={-0.0001}
-    />
+      <directionalLight
+        position={[25, 18, -25]}
+        intensity={0.7}
+        castShadow
+        shadow-camera-near={0}
+        shadow-camera-far={80}
+        shadow-camera-left={-30}
+        shadow-camera-right={30}
+        shadow-camera-top={25}
+        shadow-camera-bottom={-25}
+        shadow-mapSize-width={4096}
+        shadow-mapSize-height={4096}
+        shadow-bias={-0.0001}
+      />
       <OrbitControls />
       <Map />
+      {players.map(({state, joystick}, idx) => (
+        <CharacterController
+          key={state.id}
+          position-x={idx * 2}
+          state={state}
+          joystick={joystick}
+          usePlayer={state.id === myPlayer()?.id}
+        />
+      ))}
       <Environment preset="sunset" />
     </>
   );
